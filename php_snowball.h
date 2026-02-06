ca54dd7e0712f7cf7e2a758237101307ed169c88
rernesto <rernesto.espinosa@gmail.com>
2026-02-06 17:08:49 +0000

    chore: patch version update

--- php_snowball.h
+++ php_snowball.h
@@ -4,6 +4,6 @@
 extern zend_module_entry snowball_module_entry;
 #define phpext_snowball_ptr &snowball_module_entry
 
-#define PHP_SNOWBALL_VERSION "1.0.1"
+#define PHP_SNOWBALL_VERSION "1.0.2"
 
 #endif /* PHP_SNOWBALL_H */
